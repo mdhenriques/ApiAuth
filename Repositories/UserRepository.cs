7add15272ec720766b8d7e18c146f906b8c05afe
unisanta <unisanta@santacecilia.br>
2023-10-06 20:13:38 -0300

    Criando a classe Settings

--- Repositories/UserRepository.cs
+++ Repositories/UserRepository.cs
@@ -1,6 +1,18 @@
-﻿namespace ApiAuth.Repositories
+﻿using ApiAuth.Models;
+
+namespace ApiAuth.Repositories
 {
     public class UserRepository
     {
+        public static User Get(string username, string password)
+        {
+            var users = new List<User>
+            {
+                new User {Id = 1, Username = "Murilo", Password = "123", Role = "Gerente"},
+                new User {Id = 1, Username = "Fulano", Password = "123", Role = "Estagiario"}
+            };
+
+            return users.First(x => x.Username == username && x.Password == password) ;
+        }
     }
 }
