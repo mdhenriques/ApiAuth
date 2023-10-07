c28fe768eb4ad6c18575098abeb4d76747485a16
unisanta <unisanta@santacecilia.br>
2023-10-06 21:25:31 -0300

    Finalizando configuracao no Program.cs

--- Program.cs
+++ Program.cs
@@ -1,5 +1,35 @@
+using ApiAuth;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
 var builder = WebApplication.CreateBuilder(args);
+
+var key = Encoding.ASCII.GetBytes(Settings.Secret);
+
+builder.Services.AddAuthentication(x =>
+{
+    x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(x =>
+{
+    x.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(key)
+    };
+});
+
+builder.Services.AddAuthorization(x =>
+{
+    x.AddPolicy("Admin", policy => policy.RequireRole("Gerente"));
+    x.AddPolicy("Estag", policy => policy.RequireRole("Estagiario"));
+});
+
 var app = builder.Build();
+
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.MapGet("/", () => "Hello World!");
 
